2769b7d781646cfcf9e5d6777c16607c4413478e
Kevin Sylvestre <kevin@ksylvest.com>
2011-10-22 16:37:28 -0700

    minor cleanup to the headers

--- test/dummy/app/controllers/users_controller.rb
+++ test/dummy/app/controllers/users_controller.rb
@@ -5,39 +5,39 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-
+    
     respond_with(@users)
   end
-
-  # GET /users/1
+  
+  # GET /users/:id
   def show
     @user = User.find(params[:id])
-
+    
     respond_with(@user)
   end
-
+  
   # GET /users/new
   def new
     @user = User.new
-
+    
     respond_with(@user)
   end
-
-  # GET /users/1/edit
+  
+  # GET /users/:id/edit
   def edit
     @user = User.find(params[:id])
     
     respond_with(@user)
   end
-
+  
   # POST /users
   def create
     @user = User.create(params[:user])
-
+    
     respond_with(@user)
   end
-
-  # PUT /users/1
+  
+  # PUT /users/:id
   def update
     @user = User.find(params[:id])
     @user.attributes = params[:user]
@@ -45,13 +45,13 @@ class UsersController < ApplicationController
     
     respond_with(@user)
   end
-
-  # DELETE /users/1
+  
+  # DELETE /users/:id
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
+    
     respond_with(@user)
   end
-
+  
 end
