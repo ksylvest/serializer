7373526d144f00e3c5dc7edbbc547294a5d29558
Kevin Sylvestre <kevin@ksylvest.com>
2011-10-22 17:22:39 -0700

    reverted more hash syntax from tests

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   
   has_serialized :settings do |settings|
-    settings.define :tw_share, default: true, type: :boolean
-    settings.define :fb_share, default: true, type: :boolean
+    settings.define :tw_share, :default => true, :type => :boolean
+    settings.define :fb_share, :default => true, :type => :boolean
   end
   
 end
