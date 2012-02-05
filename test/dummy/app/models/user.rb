0e4ce11d6229fdbab8575828901d22fb65dd834d
Kevin Sylvestre <kevin@ksylvest.com>
2012-02-05 02:30:11 -0800

    renamed one serialized attribute and added many more for testing

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -3,7 +3,14 @@ class User < ActiveRecord::Base
   has_serialized :settings do |settings|
     settings.define :tw_share, :default => true, :type => :boolean
     settings.define :fb_share, :default => true, :type => :boolean
-    settings.define :tumblr_share, :default => true, :type => :boolean
+    settings.define :tb_share, :default => true, :type => :boolean
+    
+    settings.define :status, :default => :user, :type => :symbol
+    settings.define :location, :type => :string
+    
+    settings.define :percentage, :default => 0.0, :type => :float
+    
+    settings.define :permissions, :default => [], :type => :array
   end
   
 end
