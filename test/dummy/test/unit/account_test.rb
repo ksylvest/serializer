b4c62754f5d760c0fba927b611b173ce17fc6fe1
Kevin Sylvestre <kevin@ksylvest.com>
2012-10-11 23:45:01 -0700

    added a account test similar to the proposed bug

--- test/dummy/test/unit/account_test.rb
+++ test/dummy/test/unit/account_test.rb
@@ -4,22 +4,14 @@ class AccountTest < ActiveSupport::TestCase
   
   fixtures :all
   
-  test "accepts nested attributes for users" do
-    Rails.logger.info "ALPHA"
-    
-    @account = accounts(:default)
-    
-    @account.attributes = { :user_attributes => { :tw_share => false, :fb_share => false, :tb_share => true } }
-    @account.save
-    
+  test "accepts nested attributes for users" do    
+    @account = Account.create({ :user_attributes => { :tw_share => false, :fb_share => false, :tb_share => true } })
     @user = @account.user
     
     assert @user.account, "should have an account"
     assert !@user.fb_share?, "should have accepted nested attributes"
     assert !@user.tw_share?, "should have accepted nested attributes"
     assert @user.tb_share?, "should have accepted nested attributes"
-    
-    Rails.logger.info "OMEGA"
   end
   
 end
