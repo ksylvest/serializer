e6e73e963b228c7d72809b9b75318cc0e37ce225
Kevin Sylvestre <kevin@ksylvest.com>
2013-10-29 20:03:08 -0700

    compatibility support

--- test/dummy/db/migrate/20110927222742_create_users.rb
+++ test/dummy/db/migrate/20110927222742_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.references :account, index: true
+      t.references :account, :index => true
       t.string :name
       t.string :email
       t.text :settings
