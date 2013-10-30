d45946419e147166ab5e5bf64028656492f0fc5b
Kevin Sylvestre <kevin@ksylvest.com>
2013-10-29 17:31:43 -0700

    advanced

--- test/dummy/config/initializers/other.rb
+++ test/dummy/config/initializers/other.rb
@@ -1,2 +1,8 @@
-Dummy::Application.config.session_store :cookie_store, :key => '_serializer_session'
-Dummy::Application.config.secret_token = '3814b0073a5f8c77876d5baef38b180ac12c9c50f4d5fe9c82e9bb3c4105dc612f13a9b012e33c0329d55718eafb7ab1b4fefbe36117eb8f67c4427f26634c2c'
+Dummy::Application.config.session_store :cookie_store, :key => '_dummy_session'
+Dummy::Application.config.secret_key_base = '5ff1ed532fd6db8b9fb92261d486fa553599eb7e5633c347438a38a913e11c0a5f5f8e3e6d3379e496a6b4e98c5f27d3146fdc4a3367882a8b502180c3ea1dc1'
+
+ActiveSupport.on_load(:action_controller) do
+  wrap_parameters :format => [:json] if respond_to?(:wrap_parameters)
+end
+
+Rails.application.config.filter_parameters += [:password]
